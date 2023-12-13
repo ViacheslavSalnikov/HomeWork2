cc42e084d230ddf0cc4022c3645c82e7e5367daf
Viacheslav <salnikov90@yandex.ru>
2023-12-13 22:03:22 +0500

    add Homework2

--- obj/Debug/net8.0/HomeWork2.AssemblyInfo.cs
+++ obj/Debug/net8.0/HomeWork2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeWork2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c493dfdcab9052c91d37622df0211c5ee4c11090")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeWork2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeWork2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
